18bd9122bd4aeac86cb157ae26bd9bd9ff583559
meenbeese <meenbeese@tutanota.com>
2023-06-21 21:12:54 -0400

    Implement FAB to copy exported data

--- app/src/main/java/com/dwett/habits/DisplayExportActivity.kt
+++ app/src/main/java/com/dwett/habits/DisplayExportActivity.kt
@@ -1,9 +1,14 @@
 package com.dwett.habits
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class DisplayExportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,16 @@ class DisplayExportActivity : AppCompatActivity() {
 
         val textView: TextView = findViewById(R.id.export_data)
         textView.text = "Habits\n${Habit.csvHeader()}$habits\n\nEvents\n${Event.csvHeader()}$events"
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { fabView ->
+            val valueToCopy = textView.text
+
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Copied Text", valueToCopy)
+            clipboard.setPrimaryClip(clip)
+
+            Snackbar.make(fabView, "Text copied to clipboard", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
