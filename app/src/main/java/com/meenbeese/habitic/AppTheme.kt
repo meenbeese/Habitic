77fa128caf155af769497599a78feb2ee18611d9
meenbeese <meenbeese@tutanota.com>
2023-06-24 13:13:33 -0400

    Add support for quick settings tile

--- app/src/main/java/com/meenbeese/habitic/AppTheme.kt
+++ app/src/main/java/com/meenbeese/habitic/AppTheme.kt
@@ -3,7 +3,7 @@ package com.meenbeese.habitic
 import android.app.Application
 import com.google.android.material.color.DynamicColors
 
-class ThemeHabiticApp: Application() {
+class AppTheme: Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
